b529d468f3ed03ebc75792e21f413e172da4f6dd
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-03-20 00:31:38 +0100

    adding some features, f.e. getting friends list, own tweets, timeline tweets; some tweaks in authenticate

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,8 +1,45 @@
+from tweepy import API
+from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
 import twitter_credentials
+
+
+class TwitterClient():
+
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
 
 
 class TwitterStreamer():
@@ -11,20 +48,17 @@ class TwitterStreamer():
     """
 
     def __init__(self):
-        pass
+        self.twitter_authenticator = TwitterAuthenticator()
 
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         #  This handles Twitter authentication and the connection to the Twitter Streaming API.
-        listener = StdOutListener(fetched_tweets_filename)
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
-
         stream.filter(track=hash_tag_list)
 
 
-class StdOutListener(StreamListener):
+class TwitterListener(StreamListener):
     """
     This is basic listener class that just prints received tweets tp stdout
     """
@@ -43,6 +77,8 @@ class StdOutListener(StreamListener):
         return True
 
     def on_error(self, status):
+        if status == 420:
+            return False
         print(status)
 
 
@@ -50,5 +86,10 @@ if __name__ == '__main__':
     hash_tag_list = ['liverpool fc', 'lfc', ]
     fetched_tweets_filename = 'tweets.json'
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    twitter_client = TwitterClient('LFC')
+    # print(twitter_client.get_user_timeline_tweets(1))
+    print(twitter_client.get_friend_list(2))
+    # print(twitter_client.get_home_timeline_tweets(1))
+
+    # twitter_streamer = TwitterStreamer()
+    # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
