4314ab3d5a70e1215fbaa0dbd00e9489edf00ed3
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-03-20 23:19:48 +0100

    analyzing tweet data

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -4,6 +4,8 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import numpy as np
+import pandas as pd
 import twitter_credentials
 
 
@@ -14,6 +16,9 @@ class TwitterClient():
         self.twitter_client = API(self.auth)
 
         self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
 
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
@@ -82,14 +87,42 @@ class TwitterListener(StreamListener):
         print(status)
 
 
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing content from tweets
+    """
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
+
+
+
 if __name__ == '__main__':
-    hash_tag_list = ['liverpool fc', 'lfc', ]
-    fetched_tweets_filename = 'tweets.json'
 
-    twitter_client = TwitterClient('LFC')
-    # print(twitter_client.get_user_timeline_tweets(1))
-    print(twitter_client.get_friend_list(2))
-    # print(twitter_client.get_home_timeline_tweets(1))
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+    api = twitter_client.get_twitter_client_api()
 
-    # twitter_streamer = TwitterStreamer()
-    # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    tweets = api.user_timeline(screen_name="Barteckyy", count=10)
+
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+
+    print(df.head(5))
+    # hash_tag_list = ['liverpool fc', 'lfc', ]
+    # fetched_tweets_filename = 'tweets.json'
+    #
+    # twitter_client = TwitterClient('LFC')
+    # # print(twitter_client.get_user_timeline_tweets(1))
+    # print(twitter_client.get_friend_list(2))
+    # # print(twitter_client.get_home_timeline_tweets(1))
+    #
+    # # twitter_streamer = TwitterStreamer()
+    # # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
